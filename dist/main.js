2224936e4fa6c6d5fd469fc5f0e5deabf5e2f4f4
mosscodes <mossbusiness@outlook.com>
2021-07-22 15:02:21 -0400

    add webpack url-loader

--- dist/main.js
+++ dist/main.js
@@ -60,6 +60,16 @@ eval("\n\nvar isOldIE = function isOldIE() {\n  var memo;\n  return function mem
 
 /***/ }),
 
+/***/ "./src/images/ivan-torres-MQUqbmszGGM-unsplash.jpg":
+/*!*********************************************************!*\
+  !*** ./src/images/ivan-torres-MQUqbmszGGM-unsplash.jpg ***!
+  \*********************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"e607c5f29bd44f289d40.jpg\";\n\n//# sourceURL=webpack:///./src/images/ivan-torres-MQUqbmszGGM-unsplash.jpg?");
+
+/***/ }),
+
 /***/ "./src/MrDafoe-Regular.ttf":
 /*!*********************************!*\
   !*** ./src/MrDafoe-Regular.ttf ***!
@@ -67,16 +77,6 @@ eval("\n\nvar isOldIE = function isOldIE() {\n  var memo;\n  return function mem
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 eval("module.exports = __webpack_require__.p + \"7cb484b7c593238242ae.ttf\";\n\n//# sourceURL=webpack:///./src/MrDafoe-Regular.ttf?");
-
-/***/ }),
-
-/***/ "./src/images/ivan-torres-MQUqbmszGGM-unsplash.jpg":
-/*!*********************************************************!*\
-  !*** ./src/images/ivan-torres-MQUqbmszGGM-unsplash.jpg ***!
-  \*********************************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-eval("module.exports = __webpack_require__.p + \"8e3e99661e3618d282d7.jpg\";\n\n//# sourceURL=webpack:///./src/images/ivan-torres-MQUqbmszGGM-unsplash.jpg?");
 
 /***/ }),
 
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,12 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
             },
+            {
+                test: /\.(jpg|png)$/,
+                use: {
+                  loader: 'url-loader',
+                },
+              },
         ],
     },
 };
